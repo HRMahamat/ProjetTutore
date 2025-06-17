782a7c422487f85fe25a0a7a8d9ed312d7083ee6
HRMahamat <hamadrassem@gmail.com>
2025-06-17 20:05:17 +0100

    main.py

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ CLASSES = ['Acne', 'Actinic_Keratosis', 'Benign_tumors', 'Bullous', 'Candidiasis
 ZIP_FILENAME = "Hamad_Rassem_Mahamat_SkinDiseaseModel.zip"  
 with zipfile.ZipFile(ZIP_FILENAME, "r") as archive:
     h5_files = [f for f in archive.namelist() if f.lower().endswith(".h5")]
-    if not h5_files: st.error("Pas de fichier .h5 dans le ZIP !"); st.stop()
+    if not h5_files: 
+      st.error("Pas de fichier .h5 dans le ZIP !")
+      st.stop()
     h5_inside = h5_files[0]
     target_name = os.path.basename(h5_inside)
     with archive.open(h5_inside) as src, open(target_name, "wb") as dst: dst.write(src.read())
