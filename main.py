10431fe5c6cf1b614c2f415256bb274e0a22e443
HRMahamat <hamadrassem@gmail.com>
2025-06-18 00:56:06 +0100

    main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ img = (224, 224)
 CLASSES = ['Acne', 'Actinic_Keratosis', 'Benign_tumors', 'Bullous', 'Candidiasis', 'DrugEruption', 'Eczema', 'Infestations_Bites', 'Lichen', 'Lupus', 'Moles', 'Psoriasis', 'Rosacea', 'Seborrh_Keratoses', 'SkinCancer', 'Sun_Sunlight_Damage', 'Tinea', 'Unknown_Normal', 'Vascular_Tumors', 'Vasculitis', 'Vitiligo', 'Warts']
 
 # Charge modèle + historique
-MODEL_URL = "https://gitlab.com/hamadrassem-group/hamadrassem-project/-/raw/460f2345a18b18a63df496b2f3f533e76dba78cf/Hamad_Rassem_Mahamat_SkinDiseaseModel.h5"
+MODEL_URL = "https://gitlab.com/hamadrassem-group/hamadrassem-project/-/raw/main/460f2345a18b18a63df496b2f3f533e76dba78cf/Hamad_Rassem_Mahamat_SkinDiseaseModel.h5"
 LOCAL_MODEL = "Hamad_Rassem_Mahamat_SkinDiseaseModel.h5"
 
 if not os.path.exists(LOCAL_MODEL):
